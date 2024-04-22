d7cb3624b518e115cf715b1b34ebb341cd51785f
unknown <koenvanwijlick@gmail.com>
2024-04-22 15:20:59 +0200

    lessons update

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,39 @@
 import 'package:flutter/material.dart';
-
-import 'pages/home.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
-  // This widget is the root of your application.
+  List names = ["Test1", "Test2", "test3"];
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'Poppins'),
-      home: const HomePage()
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 255, 200, 100),
+        appBar: AppBar(
+          title: Text("This is my Appbar"),
+          backgroundColor: Color.fromARGB(255, 255, 200, 0),
+          elevation: 0,
+          leading: Icon(Icons.menu),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.logout, color: Colors.black,))
+          ],
+          ),
+        body: Center(
+          child: Container(
+            height: 300,
+            width:300,
+            color: Colors.yellow,
+            child: Center(child: Text("Tap me")),
+          ),
+        )
+      )
     );
   }
 }
